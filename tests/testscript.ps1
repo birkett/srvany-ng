c64a28fea7d9ba8684a93df669250656370c0f99
Anthony Birkett <anthony@a-birkett.com>
2025-12-11 07:22:14 +0000

    Add additional cases to the test script, closes #15

--- tests/testscript.ps1
+++ tests/testscript.ps1
@@ -28,6 +28,72 @@
  *  in the Parameters registry key.
 #>
 
+function Create-TestService {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $ServiceName,
+        [Parameter(Mandatory=$true)]
+        [string] $BinaryPath
+    )
+
+    # Create the service.
+    Write-Host "Creating service $ServiceName"
+    New-Service -Name $ServiceName -BinaryPathName $BinaryPath
+}
+
+function Set-TestServiceParameters {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $ServiceName,
+        [Parameter(Mandatory=$true)]
+        [string] $Application,
+        [Parameter(Mandatory=$true)]
+        [string] $AppDirectory,
+        [Parameter(Mandatory=$true)]
+        [string] $AppParameters,
+        [string[]] $AppEnvironment,
+        [Nullable[int]] $OverrideEnvironment
+    )
+
+    # Create the registry keys and populate test data.
+    Write-Host "Creating registry keys"
+
+    $regPath = "HKLM:\System\CurrentControlSet\Services\$ServiceName\Parameters"
+
+    New-Item -Path $regPath -Force | out-null
+
+    New-ItemProperty -Path $regPath -Name "Application" -PropertyType String -Value $Application | out-null
+    New-ItemProperty -Path $regPath -Name "AppDirectory" -PropertyType String -Value $AppDirectory | out-null
+    New-ItemProperty -Path $regPath -Name "AppParameters" -PropertyType String -Value $AppParameters | out-null
+
+    if ($PSBoundParameters.ContainsKey('AppEnvironment') -and $null -ne $AppEnvironment) {
+        New-ItemProperty -Path $regPath -Name "AppEnvironment" -PropertyType MultiString -Value $AppEnvironment | out-null
+    }
+
+    if ($PSBoundParameters.ContainsKey('OverrideEnvironment') -and $null -ne $OverrideEnvironment) {
+        New-ItemProperty -Path $regPath -Name "OverrideEnvironment" -PropertyType Dword -Value $OverrideEnvironment | out-null
+    }
+}
+
+function Invoke-ServiceLifecycle {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] $ServiceName
+    )
+
+    # Start the service.
+    Write-Host "Starting service $ServiceName"
+    Start-Service $ServiceName
+
+    # Stop the service if testapp hung for some reason. (When testapp closes, the service should auto stop).
+    Write-Host "Stopping service $ServiceName"
+    Stop-Service $ServiceName
+
+    # No cmdlet available for this, call sc to delete the service (will clean up the whole registry key).
+    Write-Host "Deleting service $ServiceName"
+    sc.exe delete $ServiceName
+}
+
 # Script has to run elevated, for Service install / removal and registry write access.
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
@@ -35,39 +101,61 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-# Binary paths.
-$binaryPath = $PSScriptRoot + "\..\" + "msvc\build\bin\srvany-ng\Debug Unicode_x64\srvany-ng_du64.exe"
-$testAppPath = $PSScriptRoot + "\..\" + "msvc\build\bin\testapp\Debug Unicode_x64\testapp_du64.exe"
+# Test data
+$testCases = @(
+    @{
+        Name = "No AppEnvironment - No OverrideEnvironment"
+        AppEnvironment = $null
+        OverrideEnvironment = $null
+    },
+    @{
+        Name = "No AppEnvironment - OverrideEnvironment 0"
+        AppEnvironment = $null
+        OverrideEnvironment = 0
+    },
+    @{
+        Name = "No AppEnvironment - OverrideEnvironment 1"
+        AppEnvironment = $null
+        OverrideEnvironment = 1
+    },
+    @{
+        Name = "AppEnvironment Present - No OverrideEnvironment"
+        AppEnvironment = @("SOMEENVAR=SOMEVALUE", "ANOTHERVAR=VALUE2")
+        OverrideEnvironment = $null
+    },
+    @{
+        Name = "AppEnvironment Present - OverrideEnvironment 0"
+        AppEnvironment = @("SOMEENVAR=SOMEVALUE", "ANOTHERVAR=VALUE2")
+        OverrideEnvironment = 0
+    },
+    @{
+        Name = "AppEnvironment Present - OverrideEnvironment 1"
+        AppEnvironment = @("SOMEENVAR=SOMEVALUE", "ANOTHERVAR=VALUE2")
+        OverrideEnvironment = 1
+    }
+)
 
+foreach ($case in $testCases) {
+    # Binary paths.
+    $binaryPath = $PSScriptRoot + "\..\" + "msvc\build\bin\srvany-ng\Debug Unicode_x64\srvany-ng_du64.exe"
+    $testAppPath = $PSScriptRoot + "\..\" + "msvc\build\bin\testapp\Debug Unicode_x64\testapp_du64.exe"
 
-# Test data, the output directory (AppDirectory), parameters (AppParameters) and EnvVars (AppEnvironment).
-$serviceName = "srvany-testapp"
-$testAppOutput = $PSScriptRoot + "\..\" + "tests\"
-$testAppParams = "-someparam -another param2"
-$testAppEnv = "SOMEENVAR=SOMEVALUE", "ANOTHERVAR=VALUE2"
-$overrideEnvironment = 1
+    $slugifiedCaseName = $case.Name -replace '\s+', '-'
+    $slugifiedCaseName = $slugifiedCaseName -replace '--', '-'
 
-# Create the service.
-Write-Host "Creating service $serviceName"
-New-Service -Name $serviceName -BinaryPathName $binaryPath
+    $serviceName = "srvany-testapp-" + $slugifiedCaseName
+    $testAppOutput  = $PSScriptRoot + "\..\" + "tests\"
+    $testAppParams = "-someparam -another param2"
 
-# Create the registry keys and populate test data.
-Write-Host "Creating registry keys"
-New-Item -Path HKLM:\System\CurrentControlSet\Services\$serviceName -Name "Parameters" –Force | out-null
-New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\$serviceName\Parameters -Name "Application" -PropertyType String -Value $testAppPath | out-null
-New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\$serviceName\Parameters -Name "AppDirectory" -PropertyType String -Value $testAppOutput | out-null
-New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\$serviceName\Parameters -Name "AppParameters" -PropertyType String -Value $testAppParams | out-null
-New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\$serviceName\Parameters -Name "AppEnvironment" -PropertyType MultiString -Value $testAppEnv | out-null
-New-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\$serviceName\Parameters -Name "OverrideEnvironment" -PropertyType Dword -Value $overrideEnvironment | out-null
+    Write-Host "---------------------------------------"
+    Write-Host " RUNNING TEST CASE: $slugifiedCaseName"
+    Write-Host "---------------------------------------"
 
-# Start the service.
-Write-Host "Starting service $serviceName"
-Start-Service $serviceName
+    Create-TestService -ServiceName $serviceName -BinaryPath $binaryPath
 
-# Stop the service if testapp hung for some reason. (When testapp closes, the service should auto stop).
-Write-Host "Stopping service $serviceName"
-Stop-Service $serviceName
+    Set-TestServiceParameters -ServiceName $serviceName -Application $testAppPath -AppDirectory $testAppOutput -AppParameters $testAppParams -AppEnvironment $case.AppEnvironment -OverrideEnvironment $case.OverrideEnvironment
 
-# No cmdlet available for this, call sc to delete the service (will clean up the whole registry key).
-Write-Host "Deleting service $serviceName"
-sc.exe delete $serviceName
+    Invoke-ServiceLifecycle -ServiceName $serviceName
+
+    Move-Item -Force -Path ($testAppOutput + "output.txt") -Destination ($testAppOutput + $slugifiedCaseName + ".txt")
+}
